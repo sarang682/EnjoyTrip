bbd00af41a3dfb19763e0259f5e788163d6f6f88
박사랑 <jms20914@gmail.com>
2023-11-20 14:13:13 +0900

    feat: 로그인 API

--- EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/controller/MemberController.java
+++ EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ssafy.enjoytrip.controller;
 import com.ssafy.enjoytrip.common.response.BaseResponse;
 import com.ssafy.enjoytrip.dto.member.JoinRequest;
 import com.ssafy.enjoytrip.domain.Member;
+import com.ssafy.enjoytrip.dto.member.LoginRequest;
 import com.ssafy.enjoytrip.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +17,20 @@ public class MemberController {
 
     private final MemberService service;
 
-    @GetMapping("/info/{memberId}")
-    public BaseResponse<Optional<Member>> getUserInfo(@PathVariable String memberId) {
-//        return new BaseResponse<>(service.getUerInfo(memberId));
-        return null;
-    }
-
     @PostMapping("")
     public BaseResponse<?> join(@RequestBody JoinRequest request) {
         service.join(request);
         return new BaseResponse<>(null);
+    }
+
+    @PostMapping("/login")
+    public BaseResponse<String> login(@RequestBody LoginRequest request) {
+        return new BaseResponse<>(service.login(request.getMemberId(),request.getPassword()));
+    }
+
+    @GetMapping("/info/{memberId}")
+    public BaseResponse<Optional<Member>> getUserInfo(@PathVariable String memberId) {
+//        return new BaseResponse<>(service.getUerInfo(memberId));
+        return null;
     }
 }
--- EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/service/MemberService.java
+++ EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/service/MemberService.java
@@ -1,13 +1,16 @@
 package com.ssafy.enjoytrip.service;
 
+import com.ssafy.enjoytrip.Util.JWTUtil;
 import com.ssafy.enjoytrip.common.exception.BadRequestException;
 import com.ssafy.enjoytrip.common.exception.MemberException;
 import com.ssafy.enjoytrip.common.response.ExceptionStatus;
 import com.ssafy.enjoytrip.domain.Member;
 import com.ssafy.enjoytrip.dto.member.JoinRequest;
+import com.ssafy.enjoytrip.dto.member.LoginRequest;
 import com.ssafy.enjoytrip.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +20,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final JWTUtil jwtUtil;
 
     @Transactional
     public void join(JoinRequest request) {
@@ -31,4 +35,15 @@ public class MemberService {
                        request.getEmailDomain()));
     }
 
+    public String login(String memberId, String password) {
+        // 회원가입 여부 체크
+        Member member=memberRepository.findById(memberId).orElseThrow(()-> new MemberException(ExceptionStatus.MEMBER_NOT_FOUND));
+
+        // 비밀번호 체크
+        if(!member.getPassword().equals(password)){
+            throw new MemberException(ExceptionStatus.PASSWORD_NO_MATCH);
+        }
+        // 토큰생성
+        return jwtUtil.createAccessToken(member.getId());
+    }
 }

ad68dc245516c7f09638ac689d0fedba3774695c
limsubinn <mymyx02@naver.com>
2023-11-20 10:59:09 +0900

    feat: 구군 리스트 조회

--- EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/service/AttractionService.java
+++ EnjoyTrip-BackEnd/jpa-version/src/main/java/com/ssafy/enjoytrip/service/AttractionService.java
@@ -1,7 +1,11 @@
 package com.ssafy.enjoytrip.service;
 
+import com.ssafy.enjoytrip.common.exception.AttractionException;
+import com.ssafy.enjoytrip.domain.Gugun;
 import com.ssafy.enjoytrip.domain.Sido;
+import com.ssafy.enjoytrip.dto.attraction.GetGugunResponse;
 import com.ssafy.enjoytrip.dto.attraction.GetSidoResponse;
+import com.ssafy.enjoytrip.repository.GugunRepository;
 import com.ssafy.enjoytrip.repository.SidoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,11 +13,14 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ssafy.enjoytrip.common.response.ExceptionStatus.*;
+
 @Service
 @RequiredArgsConstructor
 public class AttractionService {
 
     private final SidoRepository sidoRepository;
+    private final GugunRepository gugunRepository;
 
     public List<GetSidoResponse> getSidoList() {
         List<Sido> sidos = sidoRepository.findAll();
@@ -24,5 +31,24 @@ public class AttractionService {
         }
 
         return result;
+    }
+
+    public List<GetGugunResponse> getGugunList(int sidoCode) {
+        // 시도 코드 유효성 검사
+        findSidoByCode(sidoCode);
+
+        List<Gugun> guguns = gugunRepository.findAllBySidoCode(sidoCode);
+
+        List<GetGugunResponse> result = new ArrayList<>();
+        for (Gugun gugun: guguns) {
+            result.add(new GetGugunResponse(gugun));
+        }
+
+        return result;
+    }
+
+    public Sido findSidoByCode(int code) {
+        return sidoRepository.findByCode(code)
+                .orElseThrow(() -> new AttractionException(SIDO_NOT_FOUND));
     }
 }

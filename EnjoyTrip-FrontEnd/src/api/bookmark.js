daea8ec4191b29efa1709b0e4c28ac39ed185b16
limsubinn <mymyx02@naver.com>
2023-11-22 17:45:02 +0900

    feat: 관광지 관련 api 작성

---
+++ EnjoyTrip-FrontEnd/src/api/bookmark.js
@@ -0,0 +1,20 @@
+import { localAxios } from "@/util/http-commons";
+
+const local = localAxios();
+
+async function bookmark(request, success, fail) {
+    local.defaults.headers["Authorization"] =
+        sessionStorage.getItem("accessToken");
+    await local.post(`/bookmarks`, JSON.stringify(request)).then(success).catch(fail);
+}
+
+async function getBookmarkList(success, fail) {
+    local.defaults.headers["Authorization"] =
+        sessionStorage.getItem("accessToken");
+    await local.post(`/bookmarks`).then(success).catch(fail);
+}
+
+export {
+    bookmark,
+    getBookmarkList,
+};
